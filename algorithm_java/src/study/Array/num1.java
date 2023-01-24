9f44db9e25bc3e50a2ddf275e7c67ac74f74492b
rladuswl <khkkgon0118@gmail.com>
2023-01-24 22:49:43 +0900

    1. 큰 수 출력하기

---
+++ algorithm_java/src/study/Array/num1.java
@@ -0,0 +1,36 @@
+package study.Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 1. 큰 수 출력하기
+public class num1 {
+
+    static ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        answer.add(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i-1]) {
+                answer.add(arr[i]);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int x : solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
