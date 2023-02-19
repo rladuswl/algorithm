6916769ad865349750ff8830fb038549e9beee8d
rladuswl <khkkgon0118@gmail.com>
2023-02-20 00:01:03 +0900

    4. 연속 부분수열

---
+++ algorithm_java/src/study/TwoPointersAndSlidingWindow/num4.java
@@ -0,0 +1,40 @@
+package study.TwoPointersAndSlidingWindow;
+
+import java.util.Scanner;
+
+// 4. 연속 부분수열
+public class num4 {
+
+    static int solution(int n, int m, int[] arr) {
+        int answer = 0, sum = 0, lt = 0;
+
+        for (int rt = 0; rt < n; rt++) {
+            sum += arr[rt];
+            if (sum == m) {
+                answer++;
+            }
+            while (sum >= m) {
+                sum -= arr[lt++];
+                if (sum == m) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(n, m, arr));
+    }
+}
